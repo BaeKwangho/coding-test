933e9f14df79b7f1e107ab3dc030b711de745327
Baekwangho <qorhkdgh00@gmail.com>
2021-02-10 01:20:33 +0900

    14501 debug

--- 210209.py
+++ 210209.py
@@ -1,8 +1,6 @@
 '''
 https://www.acmicpc.net/problem/14501
-
-퇴사 문제.
-'''
+퇴사 문제. 아래 주석 부분은 오답코드.
 
 import numpy as np
 from sys import stdin
@@ -15,7 +13,6 @@ def main():
 	if not (n >=1) & (n <=15):
 		raise ValueError
 
-	
 	for x in range(0,n):
 		line = stdin.readline()
 		t, w = line.split()
@@ -28,7 +25,6 @@ def main():
 			
 		timelist.append(t)
 		worthlist.append(w)
-	
 
 	max_worth = 0
 	for x in range(0,n): # 0~14
@@ -53,6 +49,24 @@ def func(profit,max_date,cur_date):
 		profit = func(profit, max_date, cur_date)
 	return profit
 
+if __name__ == '__main__':
+	main()
+'''
+
+#정답코드
+def main():
+	n = int(input())
+	timelist, worthlist = [0]*n, [0]*n
+	for i in range(n):
+		timelist[i], worthlist[i] = map(int, input().split())
+
+	dp = [0]*75
+	for i in range(n):
+		if dp[i] > dp[i+1]:
+			dp[i+1] = dp[i]
+		if dp[i+timelist[i]] < dp[i] + worthlist[i]:
+			dp[i+timelist[i]] = dp[i] + worthlist[i]
+	print(dp[n])
 
 if __name__ == '__main__':
 	main()
